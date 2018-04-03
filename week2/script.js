7f6c029c8b1f5acb0cff33742dffcaa01f0f31c5
Shiva Moradi <36813022+ShivaMoradi@users.noreply.github.com>
2018-04-03 12:08:35 +0200

    Update script.js

--- week2/script.js
+++ week2/script.js
@@ -1,4 +1,7 @@
 //exercises-JavaScript
 console.log("Hello World!");
 console.log("Hej Världen!");
-console.log('I'm awesome');
+console.log("I'm awesome");
+var x;
+console.log("the value of my variable x will be: undefined");
+consol.log("the valu of my variable x will be:"x);
