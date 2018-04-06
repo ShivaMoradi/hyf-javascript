a86b3d277d6312f9c2fd4046a0d3e957cac0958b
Shiva Moradi <36813022+ShivaMoradi@users.noreply.github.com>
2018-04-06 14:15:09 +0200

    Update script.js

--- week2/script.js
+++ week2/script.js
@@ -1,16 +1,86 @@
 //exercises-JavaScript
+//exercise1
 console.log("Hello World!");
 console.log("Hej Världen!");
 console.log("I'm awesome");
+//exercise-2
 var x;
 console.log('the value of my variable x will be:' undefined);
 console.log('the valu of my variable x will be:' +x);
 var x=1;
 console.log("the value of x is:1");
 console.log('the value of x is:' +x);
+//exercise-3
 var y= "This is Shiva";
 console.log("the value of y is: This is Shiva");
 console.log('the value of y is:' +y);
+//exercise-4
 var y="Welcome";
 console.log("the value of y is: Welcome");
 console.log('the value of y is:' +y);
+//exercise-5
+var z=7.25;
+console.log('the value of z is:' +z);
+var a= Math.round(7.25);
+console.log(a);
+var b;
+a===z;
+b= Math.max(a,z);
+console.log(b);
+//exercise-6
+var plurality= [];
+console.log('empty array');
+console.log(plurality);
+var  favoriteAnimals= ["Dog", "Horse", "Monkey"];
+console.log(favoriteAnimals);
+favoriteAnimals.push("baby pig");
+console.log(favoriteAnimals);
+//exercise-7
+let myString = "this is a test";
+console.log(myString);
+myString.length;
+console.log(myString.length);
+//exercise-8
+var a= "shiva";
+var b= 1988;
+var c= true;
+var d= 7.12;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log("typeof variable a is string");
+console.log("typeof variable b is number");
+console.log("typeof variable c is boolean");
+console.log("typeof variable d is decimal ");
+consol.log(typeof a);
+consol.log(typeof b);
+consol.log(typeof c);
+consol.log(typeof d);
+if(typeof a = typeof b){
+  console.log("same type");
+}else{
+  console.log("not the same type");
+}
+if(typeof a = typeof d){
+  console.log("same type");
+}else{
+  console.log("not the same type");
+}
+//exercise-9
+x = 7;
+x = x % 3;
+console.log("the value of x will be:1");
+y = 6;
+y = y % 2;
+console.log("the value of y will be :0");
+z = 10;
+z = z % 100;
+console.log("the value of z will be: 10");
+console.log("% is showing precent of a number");
+//exercise-10
+var myInfo= ["shiva", 1988];
+console.log(myInfo);
+if (6/0 === 10/0){
+  console.log("same value");
+}
