027d4acaa890866d901297490f91927b3f1a4c8d
Shiva Moradi <36813022+ShivaMoradi@users.noreply.github.com>
2018-04-09 17:35:17 +0200

    Update script.js

--- week3/script.js
+++ week3/script.js
@@ -1,4 +1,27 @@
 //String and Array challenges
-//1
-let myString = "Shiva";
+//1-string
+let myString = "hello,this,is,a,difficult,to,read,sentence";
 console.log(myString);
+myString.length;
+console.log(myString.length);
+let obj = { sentence:'hello this is a difficult to read sentence'};
+console.log(obj.sentence);
+//2-array
+
+
+let favoriteAnimals = ['blowfish', 'capricorn', 'giraffe'];
+favoriteAnimals.push('turtle');
+console.log(favoriteAnimals);
+var i;
+for (i = 0; i < favoriteAnimals.length; i++){
+    if (favoriteAnimals === 'blowfish'){
+      favoriteAnimals.push('meerkat');
+    }
+  console.log(favoriteAnimals);
+}
+
+console.log(favoriteAnimals);
+
+
+
+
